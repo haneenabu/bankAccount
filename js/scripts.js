cf087d55cb0cb96c2754f59dae2f6a94d25dd309
Beth Hansen, Evan Johnson and Haneen AbuKhater <evan@>
2017-07-18 16:45:31 -0700

    refactor backend

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,35 @@
 function BankAccount (name, deposit) {
   this.accountName = name;
   this.accountBalance = deposit;
-  this.runningBalance = [];
+  this.final = deposit;
 }
 
-this.accountBalance += deposits
-
-this.accountbalance -= withdrawls
-
-function AccountChange (deposit, withdrawl, final) {
-  this.credits = deposit;
-  this.debits = withdrawl * -1;
-  this.final = final;
+BankAccount.prototype.accountDebits = function(deposit, withdrawl) {
+  return this.final + deposit - withdrawl ;
 }
 
-AccountChange.prototype.findChange = function() {
-  var z = this.final;
-  var x = this.credits;
-  var y = this.debits;
-  //If either input is not a number, ignore it and add the others
-  if (isNaN(x)) {
-    return y+z;
-  }else if (isNaN(y)) {
-    return  x+z;
-  }else {
-    return  x+y+z;
-  }
-}
+// function AccountChange (deposit, withdrawl, final) {
+//   this.credits = deposit;
+//   this.debits = withdrawl * -1;
+//   this.final = final;
+// }
+
+// AccountChange.prototype.findChange = function() {
+//   var z = this.final;
+//   var x = this.credits;
+//   var y = this.debits;
+//   //If either input is not a number, ignore it and add the others
+//   if (isNaN(x)) {
+//     return y+z;
+//   }else if (isNaN(y)) {
+//     return  x+z;
+//   }else {
+//     return  x+y+z;
+//   }
+// }
 //User Interface Logic
 $(document).ready(function() {
-var final;
+var final = [];
 
   $('#newAccount').submit(function(event) {
     event.preventDefault();
@@ -39,14 +39,13 @@ var final;
     var initialDeposit = parseInt( $('#initialDeposit').val() );
 
     var newUserAccount = new BankAccount(accountName, initialDeposit);
-
+    final.push(newUserAccount);
     //Display account name and initial deposit to user
     $('.specificName').text(newUserAccount.accountName);
     $('.specificBalance').text(newUserAccount.accountBalance);
     $('.hiddenAnswer').show();
 
     //store initial deposit amount in final variable
-    final = newUserAccount.accountBalance;
     //Clear input forms
     $('.form-control').val('');
   });
@@ -56,10 +55,18 @@ var final;
     //Take inputs from user as numbers
     var deposits = parseInt($('#depositAmount').val());
     var withdrawls = parseInt($('#withdrawlAmount').val());
-    //Create new instance of AccountChange object
 
-    var newChange = new AccountChange(deposits, withdrawls, final);
-    $('.specificBalance').text(newChange.findChange());
+
+    console.log(final[0]);
+    //Create new instance of AccountChange object
+    var runningBalance = final[0];
+    var newTotal = runningBalance.accountDebits(deposits, withdrawls);
+    // (deposits, withdrawls, final);
+    // =newUserAccount(final[0])
+    // newChange.accountDebits(deposits,withdrawls);
+    // alert(newUserAccount.final)
+    $('.specificBalance').text(newTotal);
+    //Clear input forms
     $('.form-control').val('');
   });
 });
